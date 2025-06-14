a1ce537e81ecb472d82ffddb9ecb93f1e0d6b27e
Kacper <5401764+kaqu@users.noreply.github.com>
2025-06-14 11:35:49 +0200

    feat: initialize database at startup

--- apps/desktop/src/main.rs
+++ apps/desktop/src/main.rs
@@ -1,7 +1,16 @@
-#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+mod db;
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            db::init_db(app.handle())?;
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
